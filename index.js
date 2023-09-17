a0c3c6ecf71170c67cfb049352e9d4c70bd27cb1
Munique Feitoza <muniquefeitoz4@gmail.com>
2023-09-17 14:19:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-let btn = document.querySelector("button");
+let btn4 = document.querySelector(".btn4");
 
-btn.addEventListener("click", () => {
+btn4.addEventListener("click", () => {
+  document.querySelector(".num1").innerHTML = 
+    Math.floor(Math.random() * 4 ) + 1;
+  document.querySelector(".num2").innerHTML = 
+    Math.floor(Math.random() * 4 ) + 1;
+  document.querySelector(".num3").innerHTML = 
+    Math.floor(Math.random() * 4 ) + 1;
+ }
+);
+
+let btn6 = document.querySelector(".btn6");
+
+btn6.addEventListener("click", () => {
   document.querySelector(".num1").innerHTML = 
     Math.floor(Math.random() * 6 ) + 1;
   document.querySelector(".num2").innerHTML = 
     Math.floor(Math.random() * 6 ) + 1;
   document.querySelector(".num3").innerHTML = 
     Math.floor(Math.random() * 6 ) + 1;
+ }
+);
+
+let btn8 = document.querySelector(".btn8");
+
+btn8.addEventListener("click", () => {
+  document.querySelector(".num1").innerHTML = 
+    Math.floor(Math.random() * 8 ) + 1;
+  document.querySelector(".num2").innerHTML = 
+    Math.floor(Math.random() * 8 ) + 1;
+  document.querySelector(".num3").innerHTML = 
+    Math.floor(Math.random() * 8 ) + 1;
+ }
+);
+
+let btn12 = document.querySelector(".btn12");
+
+btn12.addEventListener("click", () => {
+  document.querySelector(".num1").innerHTML = 
+    Math.floor(Math.random() * 12 ) + 1;
+  document.querySelector(".num2").innerHTML = 
+    Math.floor(Math.random() * 12 ) + 1;
+  document.querySelector(".num3").innerHTML = 
+    Math.floor(Math.random() * 12 ) + 1;
+ }
+);
+
+let btn20 = document.querySelector(".btn20");
+
+btn20.addEventListener("click", () => {
+  document.querySelector(".num1").innerHTML = 
+    Math.floor(Math.random() * 20 ) + 1;
+  document.querySelector(".num2").innerHTML = 
+    Math.floor(Math.random() * 20 ) + 1;
+  document.querySelector(".num3").innerHTML = 
+    Math.floor(Math.random() * 20 ) + 1;
  }
 );
